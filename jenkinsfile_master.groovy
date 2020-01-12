c58ce4adee12ab5f9be1e6db16ab56c8e8cbc6cb
an5456 <527011764@qq.com>
2020-01-12 11:23:43 +0800

    123

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -1,6 +1,6 @@
 stage('pull source code') {
     node('master'){
-        git([url: 'git@github.com:princeqjzh/iWeb.git', branch: 'master'])
+        git([url: 'git@github.com:an5456/iWeb.git', branch: 'master'])
     }
 }
 
